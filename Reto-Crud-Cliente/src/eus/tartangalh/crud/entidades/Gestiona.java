a046d55cf6844c0e6a5853da6b683016083b2f7b
markel <markelarabiourrutia@gmail.com>
2025-01-10 13:14:39 +0100

    Entidades

---
+++ Reto-Crud-Cliente/src/eus/tartangalh/crud/entidades/Gestiona.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eus.tartangalh.crud.entidades;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author 2dam
+ */
+@XmlRootElement
+public class Gestiona implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private GestionaId gestionaId;
+
+    private Trabajador trabajador;
+
+    private ProductoFarmaceutico productoFarmaceutico;
+    private LocalDate fechaCompra;
+
+    private Integer cantidad;
+
+    public Gestiona(GestionaId gestionaId, Trabajador trabajador, ProductoFarmaceutico productoFarmaceutico, Integer cantidad) {
+        this.gestionaId = gestionaId;
+        this.trabajador = trabajador;
+        this.productoFarmaceutico = productoFarmaceutico;
+        this.cantidad = cantidad;
+    }
+
+    public Gestiona() {
+    }
+
+    public LocalDate getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(LocalDate fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public GestionaId getGestionaId() {
+        return gestionaId;
+    }
+
+    public void setGestionaId(GestionaId gestionaId) {
+        this.gestionaId = gestionaId;
+    }
+
+    public Trabajador getTrabajador() {
+        return trabajador;
+    }
+
+    public void setTrabajador(Trabajador trabajador) {
+        this.trabajador = trabajador;
+    }
+
+    public ProductoFarmaceutico getProductoFarmaceutico() {
+        return productoFarmaceutico;
+    }
+
+    public void setProductoFarmaceutico(ProductoFarmaceutico productoFarmaceutico) {
+        this.productoFarmaceutico = productoFarmaceutico;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (gestionaId != null ? gestionaId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Gestiona)) {
+            return false;
+        }
+        Gestiona other = (Gestiona) object;
+        if ((this.gestionaId == null && other.gestionaId != null) || (this.gestionaId != null && !this.gestionaId.equals(other.gestionaId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Gestiona[ id=" + gestionaId + " ]";
+    }
+
+}
---
+++ Reto-Crud-Cliente/src/eus/tartangalh/crud/entidades/Proveedor.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eus.tartangalh.crud.entidades;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author 2dam
+ */
+
+@XmlRootElement
+public class Proveedor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+ 
+    private Integer idProveedor;
+    private String cif;
+    private String nombreProveedor;
+    private String calle;
+    private Integer codPostal;
+    private String ciudad;
+    private LocalDate fechaContratacion;
+
+ 
+    private List<ProductoFarmaceutico> productoFarmaceutico;
+
+    public Proveedor(Integer idProveedor, String cif, String nombreProveedor, String calle, Integer codPostal, String ciudad, LocalDate fechaContratacion, List<ProductoFarmaceutico> productoFarmaceutico) {
+        this.idProveedor = idProveedor;
+        this.cif = cif;
+        this.nombreProveedor = nombreProveedor;
+        this.calle = calle;
+        this.codPostal = codPostal;
+        this.ciudad = ciudad;
+        this.fechaContratacion = fechaContratacion;
+        this.productoFarmaceutico = productoFarmaceutico;
+    }
+
+    public Proveedor() {
+    }
+
+    public Integer getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(Integer idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public Integer getCodPostal() {
+        return codPostal;
+    }
+
+    public void setCodPostal(Integer codPostal) {
+        this.codPostal = codPostal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public LocalDate getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(LocalDate fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    @XmlTransient
+    public List<ProductoFarmaceutico> getProductoFarmaceutico() {
+        return productoFarmaceutico;
+    }
+
+    public void setProductoFarmaceutico(List<ProductoFarmaceutico> productoFarmaceutico) {
+        this.productoFarmaceutico = productoFarmaceutico;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idProveedor != null ? idProveedor.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Proveedor)) {
+            return false;
+        }
+        Proveedor other = (Proveedor) object;
+        if ((this.idProveedor == null && other.idProveedor != null) || (this.idProveedor != null && !this.idProveedor.equals(other.idProveedor))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "eus.tartangalh.crud.create.Proveedor[ id=" + idProveedor + " ]";
+    }
+
+}
