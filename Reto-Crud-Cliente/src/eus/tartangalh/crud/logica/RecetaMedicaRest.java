c0ea63a10687586bf2b9af198653db30678376c9
lucia9928 <lucia2801salvas@gmail.com>
2025-01-31 11:17:47 +0100

    modificaciones en las clases y servidor funcional

---
+++ Reto-Crud-Cliente/src/eus/tartangalh/crud/logica/RecetaMedicaRest.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eus.tartangalh.crud.logica;
+
+import eus.tartangalh.crud.interfaces.RecetaMedicaInterfaz;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+
+/**
+ * Jersey REST client generated for REST resource:RecetaMedicaFacadeREST
+ * [Receta_Medica]<br>
+ * USAGE:
+ * <pre>
+ *        RecetaMedicaRest client = new RecetaMedicaRest();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author lucia_puwj3zw
+ */
+public class RecetaMedicaRest implements RecetaMedicaInterfaz{
+
+    private final WebTarget webTarget;
+    private final Client client;
+    private static final String BASE_URI = "http://localhost:8080/Reto-crud-server/webresources";
+
+    public RecetaMedicaRest() {
+        client = javax.ws.rs.client.ClientBuilder.newClient();
+        webTarget = client.target(BASE_URI).path("Receta_Medica");
+    }
+
+    @Override
+    public void eliminarRecetamedica(String id) throws WebApplicationException {
+        webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{id})).request().delete();
+    }
+@Override
+    public <T> T obtenerProductosPorReceta(GenericType<T> responseType, String id) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("productos/{0}", new Object[]{id}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+    }
+@Override
+    public void modificarRecetaMedica_XML(Object requestEntity) throws WebApplicationException {
+        webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
+    }
+@Override
+    public void modificarRecetaMedica_JSON(Object requestEntity) throws WebApplicationException {
+        webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON));
+    }
+@Override
+    public <T> T encontrarRecetaPorId_XML(GenericType<T> responseType, String id) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+    }
+@Override
+    public <T> T encontrarRecetaPorId_JSON(GenericType<T> responseType, String id) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+@Override
+    public <T> T encontrarTodasLasRecetas_XML(GenericType<T> responseType) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+    }
+@Override
+    public <T> T encontrarTodasLasRecetas_JSON(GenericType<T> responseType) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+@Override
+    public void crearRecetaMedica_XML(Object requestEntity, String id) throws WebApplicationException {
+        webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{id})).request(javax.ws.rs.core.MediaType.APPLICATION_XML).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
+    }
+@Override
+    public void crearRecetaMedica_JSON(Object requestEntity, String id) throws WebApplicationException {
+        webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{id})).request(javax.ws.rs.core.MediaType.APPLICATION_JSON).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON));
+    }
+@Override
+    public <T> T encontrarRecetasPorFecha_XML(GenericType<T> responseType, String fechaInicio, String fechaFin) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("fecha/{0}/{1}", new Object[]{fechaInicio, fechaFin}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+    }
+@Override
+    public <T> T encontrarRecetasPorFecha_JSON(GenericType<T> responseType, String fechaInicio, String fechaFin) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("fecha/{0}/{1}", new Object[]{fechaInicio, fechaFin}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+@Override
+    public void close() {
+        client.close();
+    }
+    
+}
